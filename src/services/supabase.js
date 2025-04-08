c892e91094cf7f523dfe80fd32d9fc4e8d8ceeae
Mos3aB696 <mos3ab696@gmail.com>
2025-04-08 09:50:53 +0200

    some edits

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://euncsoojmpzbtqrbxjxh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1bmNzb29qbXB6YnRxcmJ4anhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NzMyNTcsImV4cCI6MjA1OTU0OTI1N30.xK8lM_kpjDfUVr7mWuO_DKEGsn58pC83YWpfw5Cwha0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
